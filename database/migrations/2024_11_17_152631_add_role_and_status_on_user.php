3633be8ecbbf239e2d3d27c46ce37dd8c3933e3d
Muhereza-Joel <muherezajoel40@gmail.com>
2024-11-17 18:36:44 +0300

    add role and status on users

---
+++ database/migrations/2024_11_17_152631_add_role_and_status_on_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['Admin', 'Accountant', 'Loan Officer', 'Member', 'Root'])->after('password')->default('Member');
+            $table->enum('status', ['Active', 'Inactive'])->after('role')->default('Active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('status');
+        });
+    }
+};
